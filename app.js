c52113b5e2a915d6aa4a0bebd08312a7be700901
Cody-Wise <cody@sharptackmedia.com>
2022-03-18 10:39:11 -0700

    3rd option is now working

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { renderPollsEl } from './render-functions.js';
 const questionInputEl = document.querySelector('#questioninput');
 const option1InputEl = document.querySelector('#option1input');
 const option2InputEl = document.querySelector('#option2input');
-// const option3InputEl = document.querySelector('#option3input');
+const option3InputEl = document.querySelector('#option3input');
 const currentPollDestinationEl = document.querySelector('#current-poll-destination');
 const option1ButtonAddEl = document.querySelector('#option1plus');
 const option1ButtonMinusEl = document.querySelector('#option1minus');
@@ -15,8 +15,8 @@ const pastPollDestinationEl = document.querySelector('#past-poll-destination');
 const submitButtonEl = document.querySelector('#submit-button');
 const publishButtonEl = document.querySelector('#publish-poll');
 const dropDownEl = document.querySelector('#dropdown');
-// const option3ButtonAddEl = document.querySelector('#option3plus');
-// const option3ButtonMinusEl = document.querySelector('#option3minus');
+const option3ButtonAddEl = document.querySelector('#option3plus');
+const option3ButtonMinusEl = document.querySelector('#option3minus');
 const hidden1El = document.querySelector('.hidden1');
 const hidden2El = document.querySelector('.hidden2');
 const hidden3El = document.querySelector('.hidden3');
@@ -32,10 +32,10 @@ const hidden5El = document.querySelector('.hidden5');
 let question = '';
 let option1 = '';
 let option2 = '';
-// let option3 = '';
+let option3 = '';
 let option1Votes = 0;
 let option2Votes = 0;
-// let option3Votes = 0;
+let option3Votes = 0;
 const pastPollsArray = [];
 
 dropDownEl.addEventListener('change', () => {
@@ -64,10 +64,12 @@ dropDownEl.addEventListener('change', () => {
 submitButtonEl.addEventListener('click', () => {
 
     question = questionInputEl.value;
-
+        
+    
     option1 = option1InputEl.value;
     option2 = option2InputEl.value;
-
+    option3 = option3InputEl.value;
+    
     displayCurrentPoll();
 
     
@@ -93,10 +95,24 @@ option2ButtonAddEl.addEventListener('click', () => {
     displayCurrentPoll();
     
 });
-  
+
 option2ButtonMinusEl.addEventListener('click', () => {
 
     option2Votes--;
+    displayCurrentPoll();
+    
+});
+
+option3ButtonAddEl.addEventListener('click', () => {
+
+    option3Votes++;
+    displayCurrentPoll();
+    
+});
+  
+option3ButtonMinusEl.addEventListener('click', () => {
+
+    option3Votes--;
     displayCurrentPoll();
     
 });
@@ -111,6 +127,7 @@ publishButtonEl.addEventListener('click', () => {
         option2: option2,
         option1Votes: option1Votes,
         option2Votes: option2Votes,
+        option3Votes: option3Votes,
     };
     
     // renderPollsEl(question, option1, option2, option1Votes, option2Votes);
@@ -124,6 +141,7 @@ publishButtonEl.addEventListener('click', () => {
     currentPollDestinationEl.textContent = '';
     option1Votes = 0;
     option2Votes = 0;
+    option3Votes = 0;
 
  
 
@@ -140,7 +158,7 @@ function displayAllPolls(){
     for (let pastPoll of pastPollsArray) {
         // make some stuff
         // when you create a DOM element, lets call that RENDER
-        const container = renderPollsEl(pastPoll.question, pastPoll.option1, pastPoll.option2, pastPoll.option1Votes, pastPoll.option2Votes);
+        const container = renderPollsEl(pastPoll.question, pastPoll.option1, pastPoll.option2, pastPoll.option3, pastPoll.option1Votes, pastPoll.option2Votes, pastPoll.option3Votes);
   
         pastPollDestinationEl.append(container);
 
@@ -159,7 +177,7 @@ function displayAllPolls(){
 
 function displayCurrentPoll() {
 
-    const pollEl = renderPollsEl(question, option1, option2, option1Votes, option2Votes);
+    const pollEl = renderPollsEl(question, option1, option2, option3, option1Votes, option2Votes, option3Votes);
 
     currentPollDestinationEl.textContent = '';
 
